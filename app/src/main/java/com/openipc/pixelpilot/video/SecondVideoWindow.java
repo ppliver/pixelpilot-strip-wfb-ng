061bcfe27b5710452a50fbd05f31016b244a976b
PixelPilot Dev <pixelpilot-dev@example.com>
2026-09-03 14:49:49 +0800

    fix(second-video): add missing android.util.Log import in SecondVideoWindow

--- app/src/main/java/com/openipc/pixelpilot/video/SecondVideoWindow.java
+++ app/src/main/java/com/openipc/pixelpilot/video/SecondVideoWindow.java
@@ -1,6 +1,7 @@
 package com.openipc.pixelpilot.video;
 
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
